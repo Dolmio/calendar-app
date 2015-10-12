0138cd0dc99b389807fcd056ca69af1f762698f4
Juho Salmio <juho.salmio@gmail.com>
2015-10-12 20:08:15 +0300

    add validation for attendees array

--- calendarEvent.js
+++ calendarEvent.js
@@ -1,8 +1,11 @@
 const validate = require('validate.js');
 const moment = require('moment');
+const R = require('ramda');
 
 validate.moment = moment;
 validate.validators.before = beforeValidator;
+validate.validators.emailArray = emailArray;
+
 
 const constraints =  {
   _id:{},
@@ -20,8 +23,21 @@ const constraints =  {
   endTime: {
     presence: true,
     datetime: true
+  },
+
+  attendees: {
+    emailArray: true
   }
+
 };
+
+function emailArray(value, options, key, attributes){
+  if (value &&
+    !(R.isArrayLike(value) &&
+      R.all((element) => validate.validators.email.PATTERN.test(element), value))){
+    return value + " must be array of emails";
+  }
+}
 
 function beforeValidator(value, options, key, attributes) {
   const other = attributes[options];
